87ada2b5fbb830d2d56f3a2d2f07340b7aad40db
François Pluchino <francois.pluchino@klipper.dev>
2020-05-22 18:40:18 +0200

    Fix name of the root tree builder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -30,7 +30,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('klipper_security');
+        $treeBuilder = new TreeBuilder('klipper_security_extra');
         /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->getRootNode();
         $rootNode
